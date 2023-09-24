b1c85b8ccb599eb25f97cd98ae5765f2c0bdcd49
shubhambartwal <shubhambartwal58@gmail.com>
2023-09-24 17:16:56 +0530

    added new food list

---
+++ food-app-ui/src/assets/AllMeals.js
@@ -0,0 +1,64 @@
+const DUMMY_MEALS = [
+    {
+      id: "m1",
+      name: "Butter Chicken",
+      description: "Creamy and flavorful",
+      price: 899,
+    },
+    {
+      id: "m2",
+      name: "Paneer Tikka",
+      description: "Spicy grilled cottage cheese",
+      price: 799,
+    },
+    {
+      id: "m3",
+      name: "Masala Dosa",
+      description: "Crispy South Indian delight",
+      price: 599,
+    },
+    {
+      id: "m4",
+      name: "Biryani",
+      description: "Fragrant rice dish with spices",
+      price: 799,
+    },
+    {
+      id: "m5",
+      name: "Chole Bhature",
+      description: "Puffy bread with spicy chickpeas",
+      price: 450,
+    },
+    {
+      id: "m6",
+      name: "Rogan Josh",
+      description: "Rich and aromatic lamb curry",
+      price: 1199,
+    },
+    {
+      id: "m7",
+      name: "Aloo Paratha",
+      description: "Stuffed potato flatbread",
+      price: 499,
+    },
+    {
+      id: "m8",
+      name: "Palak Paneer",
+      description: "Spinach and cottage cheese curry",
+      price: 749,
+    },
+    {
+      id: "m9",
+      name: "Tandoori Chicken",
+      description: "Marinated and grilled chicken",
+      price: 899,
+    },
+    {
+      id: "m10",
+      name: "Gulab Jamun",
+      description: "Sweet fried dumplings in sugar syrup",
+      price: 299,
+    },
+  ];
+  
+  export default DUMMY_MEALS;
